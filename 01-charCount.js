e3ba545d0c4a8400587ee314bc7880e4ec252e33
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-15 16:56:04 +0800

    add frequency counter

---
+++ 01-charCount.js
@@ -0,0 +1,10 @@
+function charCount(str) {
+	var obj = {};
+	for (var char of str) {
+		char = char.toLowerCase();
+		if (/[a-z0-9]/.test(char)) {
+			obj[char] = ++obj[char] || 1;
+		}
+	}
+	return obj;
+}
