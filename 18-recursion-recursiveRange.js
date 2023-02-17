ca3d050eb6e412ec6db07ac4cf240f92457cd359
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-17 12:18:13 +0800

    add practice 13

---
+++ 18-recursion-recursiveRange.js
@@ -0,0 +1,17 @@
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function 
+
+function recursiveRange (num) {
+    return (1 + num) * num / 2;
+}
+
+// function recursiveRange (num) {
+//     if (num === 0 ) return 0;
+//     if (num === 1 ) return num;
+
+//     let sum;
+
+//     function add (num) {
+//         sum += num;
+//     }
+
+// }
