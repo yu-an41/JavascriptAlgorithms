3e3cf17596e849bc7b92a7b7c303be4479d22904
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-18 10:08:04 +0800

    add pure recursion

--- 21-recursion-isPalindrome.js
+++ 21-recursion-isPalindrome.js
@@ -1,10 +1,18 @@
 // Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
 
-function isPalindrome (string) {
-    function isPalindromeHelper (str) {
-        if (str.length <= 1) return true;
-        if (str[0] !== str[str.length - 1]) return false;
-        return isPalindromeHelper(str.substring(1, str.length-1));
-    }
-    return isPalindromeHelper (string);
+// function isPalindrome (string) {
+//     function isPalindromeHelper (str) {
+//         if (str.length <= 1) return true;
+//         if (str[0] !== str[str.length - 1]) return false;
+//         return isPalindromeHelper(str.substring(1, str.length-1));
+//     }
+//     return isPalindromeHelper (string);
+// }
+
+function isPalindrome (str){
+    if (str.length <= 1) return true;
+
+    if (str[0] !== str[str.length - 1]) return false;
+
+    return isPalindrome(str.substring(1, str.length-1));
 }
