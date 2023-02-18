a3b3d40a6d6f2f2ab0311c780b590f021fa23376
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-18 09:44:54 +0800

    add pure recursion

--- 20-recursion-reverse.js
+++ 20-recursion-reverse.js
@@ -1,19 +1,32 @@
 // Write a recursive function called reverse which accepts a string and returns a new string in reverse.
 
+// function reverse (string) {
+//     let reverserArr =[];
+//     if (string.length === 0) return reverseArr.join('');
+
+//     function addReverse (string) {
+//         if (string.length === 0) return;
+
+//         reverserArr.push(string[string.length - 1]);
+
+//         return addReverse(string.substring(0, string.length -1))
+//     }
+
+//     addReverse(string);
+
+//     return reverserArr.join('');
+// }
+
 function reverse (string) {
-    if (string.length === 0) return;
+    let reverseArr = [];
 
-    let reverserArr =[];
+    if (string.length === 0) return reverseArr.join('');
 
-    function addReverse (string) {
-        if (string.length === 0) return;
+    reverseArr.push(string[string.length - 1]);
+    reverseArr = reverseArr.concat(reverse(string.substring(0, string.length - 1)));
 
-        reverserArr.push(string[string.length - 1]);
+    return reverseArr.join('');
 
-        return addReverse(string.substring(0, string.length -1))
-    }
-
-    addReverse(string);
-
-    return reverserArr.join('');
 }
+
+reverse('awesome') // 'emosewa'
