1a40a49917473cad6127c94753be9ed4e1a5de4c
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-20 22:16:04 +0800

    add mergeSort()

--- 35-mergeSort.js
+++ 35-mergeSort.js
@@ -1,25 +1,37 @@
-function merge (arr1, arr2) {
+// Merges two already sorted arrays
+function merge(arr1, arr2){
     let results = [];
     let i = 0;
     let j = 0;
-
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
             results.push(arr1[i]);
             i++;
         } else {
-            results.push(arr2[j]);
+            results.push(arr2[j])
             j++;
         }
-    }   
-    while (i < arr1.length) {
-        results.push(arr1[i]);
+    }
+    while(i < arr1.length) {
+        results.push(arr1[i])
         i++;
     }
-    while (j < arr2.length) {
-        results.push(arr2[j]);
+    while(j < arr2.length) {
+        results.push(arr2[j])
         j++;
     }
     return results;
 }
-merge([1, 10, 50], [2, 14, 99, 100]);
+// merge([100,200], [1,2,3,5,6])
+
+// Recrusive Merge Sort
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+mergeSort([10,24,76,73])
