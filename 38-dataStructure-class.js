7d1c8e2a2a111b388e05b520b41e1b294f6775bd
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-25 11:19:31 +0800

    add class

--- 38-dataStructure-class.js
+++ 38-dataStructure-class.js
@@ -28,7 +28,30 @@ class Student {
         let sum = this.scores.reduce((a, b)=> a+b)
         return sum/this.scores.length;
     }
+
+    static enrollStudents(...students) {
+        return `Enrolling students!`
+    }
 }
 
 let firstStudent = new Student('colt', 'Steele', 1);
 let secondStudent = new Student('Blue', 'Steele', 2);
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+
+        return Math.hypot(dx, dy);
+    }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+console.log(Point.distance(p1, p2)); // 7.0710678118654755
