5e2d5627b6f18de1c166c8f85433b0270514825d
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-25 22:31:16 +0800

    add pop

--- 39-singlyLinksLists.js
+++ 39-singlyLinksLists.js
@@ -1,10 +1,10 @@
 // piece of data - val
 // reference to next node - next
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
 // let first  = new Node("Hi");
@@ -13,31 +13,48 @@ class Node {
 // first.next.next.next = new Node("are");
 // first.next.next.next.next = new Node("you");
 
-class SinglyLinkedList{
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    push(val) {
-        let newNode = new Node(val);
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+
+  pop(val) {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = this.head;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push('hi');
-list.push('you');
-
+list.push("hi");
+list.push("you");
 
 // 我的push練習
 // push(val) {
@@ -50,4 +67,13 @@ list.push('you');
 //     }
 //     this.length++;
 //     // 忘記 return
+// }
+
+// traverse（模擬pop）
+// traverse(){
+//     let current = this.head;
+//     while(current) {
+//         console.log(current.val);
+//         current = current.next;
+//     }
 // }
