a25132aa3d05ac1dc80aea3f55691397ab52ef52
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-26 08:35:52 +0800

    add unshift

--- 39-singlyLinksLists.js
+++ 39-singlyLinksLists.js
@@ -50,6 +50,28 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return currentHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
