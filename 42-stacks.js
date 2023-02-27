5b89f06330e5cc900d3940d021f1e9c6041bb683
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-27 16:27:29 +0800

    add push

--- 42-stacks.js
+++ 42-stacks.js
@@ -1,7 +1,27 @@
-const stack = [];
-stack.push('google');
-stack.push('instagram');
-stack.push('facebook');
-stack.pop();
-stack.pop();
-stack.pop();
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(val) {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+}
