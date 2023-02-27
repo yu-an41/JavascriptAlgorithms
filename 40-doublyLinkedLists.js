aecbc55b9e6ec7703c9184b61d9b4918c0849413
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-27 12:12:48 +0800

    add get/set

--- 40-doublyLinkedLists.js
+++ 40-doublyLinkedLists.js
@@ -70,5 +70,57 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if (index <= this.length/2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
+// get(index) {
+//     if (index < 0 || index >= this.length) return null;
+    
+//     let middle = Math.floor(this.length / 2);
+
+//         while (counter <= middle) {
+//             let counter = 0;
+//             let current = this.head;
+//             current = current.next;
+//             counter++;
+//             if (counter === index) return current;
+//         }
+
+//         while (counter > middle) {
+//             let counter = this.length - 1;
+//             let current = this.tail;
+//             current = current.prev;
+//             counter--;
+//             if (counter === index) return current;
+//         }
+
+// }
