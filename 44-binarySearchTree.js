9f1290d4b69fa9c8aaf4e5dce5e93eed8c16916b
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-27 22:33:38 +0800

    add BinarySearchTree

---
+++ 44-binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(val) {
+        this.root = val;
+    }
+}
