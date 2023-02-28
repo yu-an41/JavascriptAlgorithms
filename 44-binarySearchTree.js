dce34d5e7e8a1539594508c46a69c079259ab7d2
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-28 12:19:14 +0800

    add insert

--- 44-binarySearchTree.js
+++ 44-binarySearchTree.js
@@ -10,4 +10,36 @@ class BinarySearchTree {
     constructor(val) {
         this.root = val;
     }
+
+    insert(val) {
+        const newNode = new Node(val);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.val) return undefined;
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
