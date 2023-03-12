adf4813404f0e6f5b607b167b7186f99c3b94c32
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-03-12 14:30:28 +0800

    add remove/ sinkDown

--- 45-BinaryHeap.js
+++ 45-BinaryHeap.js
@@ -23,7 +23,44 @@ class MaxBinaryHeap {
     }
 
     remove() {
-        
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true) {
+            let leftIdx = 2 * idx + 1;
+            let rightIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftIdx < length) {
+                leftChild = this.values[leftIdx];
+                if (leftChild > element) {
+                    swap = leftIdx;
+                }
+            }
+            if(rightIdx < length) {
+                rightChild = this.values[rightIdx];
+                if ((swap === null && rightChild > element) || (swap !== null && rightChild < leftChild)) {
+                    swap = rightIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+
+        }
+
     }
 }
 
