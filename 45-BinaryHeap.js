a5bb38e4cf6a82b662f5a8cf6b5033287484bf35
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-03-13 08:42:43 +0800

    add remove/ sinkDown

--- 45-BinaryHeap.js
+++ 45-BinaryHeap.js
@@ -64,4 +64,76 @@ class MaxBinaryHeap {
     }
 }
 
-let heap = new MaxBinaryHeap();
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    insert(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(val);
+        this.bubbleUp();
+        
+    }
+
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx -1) / 2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    remove() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true) {
+            let leftIdx = 2 * idx + 1;
+            let rightIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftIdx < length) {
+                leftChild = this.values[leftIdx];
+                if (leftChild > element) {
+                    swap = leftIdx;
+                }
+            }
+            if(rightIdx < length) {
+                rightChild = this.values[rightIdx];
+                if ((swap === null && rightChild > element) || (swap !== null && rightChild < leftChild)) {
+                    swap = rightIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+
+        }
+
+    }
+}
